c68c4e65b45cef4bf08f6b67cf6489174058437f
Neko <neko@ayaka.moe>
2025-03-15 13:39:49 +0800

    chore: update sdk/typescript/tsup.config.ts
    Co-authored-by: 藍+85CD <50108258+kwaa@users.noreply.github.com>

--- sdk/typescript/tsup.config.ts
+++ sdk/typescript/tsup.config.ts
@@ -1,12 +1,8 @@
-import { resolve } from 'node:path'
-import { cwd } from 'node:process'
 import { defineConfig } from 'tsup'
-
-const workspaceDir = cwd()
 
 export default defineConfig({
   dts: true,
   entry: ['src/index.ts'],
   format: 'esm',
-  tsconfig: resolve(workspaceDir, 'tsconfig.json'),
+  tsconfig: 'tsconfig.json',
 })
