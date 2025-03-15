7eeaf5a2bf57b1ce1351821d5ea9ca1df7e83d4e
Neko Ayaka <neko@ayaka.moe>
2025-03-15 21:41:35 +0800

    fix(sdk/typescript): to snake & export
    Co-authored-by: 藍+85CD <50108258+kwaa@users.noreply.github.com>

--- sdk/typescript/src/index.ts
+++ sdk/typescript/src/index.ts
@@ -1,3 +1,5 @@
-export * from './backend'
-export * from './types'
-export * from './utils/list-voices'
+export type * from './backend'
+export { createUnElevenLabs, createUnMicrosoft, createUnSpeech } from './backend'
+export type * from './types'
+export type * from './utils/list-voices'
+export { listVoices } from './utils/list-voices'
