43c75e92aa546359f9706afb02d79d1c214de40e
Neko <neko@ayaka.moe>
2025-03-15 13:40:18 +0800

    chore: update sdk/typescript/src/utils/list-voices.ts
    Co-authored-by: 藍+85CD <50108258+kwaa@users.noreply.github.com>

--- sdk/typescript/src/utils/list-voices.ts
+++ sdk/typescript/src/utils/list-voices.ts
@@ -12,14 +12,11 @@ export interface ListVoicesResponse {
   voices: Voice[]
 }
 
-export const listVoices = async (options: ListVoicesOptions): Promise<Voice[]> => {
-  const fetch = (options.fetch ?? globalThis.fetch)
-
-  return fetch(requestURL(options.query ? `api/voices?${options.query}` : 'api/voices', options.baseURL), {
+export const listVoices = async (options: ListVoicesOptions): Promise<Voice[]> =>
+  (options.fetch ?? globalThis.fetch)(requestURL(options.query ? `api/voices?${options.query}` : 'api/voices', options.baseURL), {
     headers: requestHeaders({ ...options.headers }, options.apiKey),
     method: 'GET',
     signal: options.abortSignal,
   })
     .then(responseJSON<ListVoicesResponse>)
     .then(({ voices }) => voices)
-}
