391bc4406c3f3c31d717a4d64d1dcb1df3fc9e0d
Neko <neko@ayaka.moe>
2025-03-18 17:02:34 +0800

    fix(microsoft): not able to inherit error message from upstream (#22)

--- pkg/backend/microsoft/voices.go
+++ pkg/backend/microsoft/voices.go
@@ -2,14 +2,12 @@ package microsoft
 
 import (
 	"encoding/json"
-	"log/slog"
 	"net/http"
 	"strings"
 
 	"github.com/labstack/echo/v4"
 	"github.com/moeru-ai/unspeech/pkg/apierrors"
 	"github.com/moeru-ai/unspeech/pkg/backend/types"
-	"github.com/moeru-ai/unspeech/pkg/utils"
 	"github.com/nekomeowww/xo"
 	"github.com/samber/mo"
 )
@@ -99,7 +97,7 @@ func HandleVoices(c echo.Context, options mo.Option[types.VoicesRequestOptions])
 
 	req.Header.Set("Ocp-Apim-Subscription-Key", strings.TrimPrefix(c.Request().Header.Get("Authorization"), "Bearer "))
 
-	res, err := http.DefaultClient.Do(req)
+	res, err := httpClient.Do(req)
 	if err != nil {
 		return mo.Err[any](apierrors.NewErrBadGateway().WithError(err).WithCaller())
 	}
@@ -107,21 +105,9 @@ func HandleVoices(c echo.Context, options mo.Option[types.VoicesRequestOptions])
 	defer func() { _ = res.Body.Close() }()
 
 	if res.StatusCode >= 400 && res.StatusCode < 600 {
-		switch {
-		case strings.HasPrefix(res.Header.Get("Content-Type"), "application/json"):
-			return mo.Err[any](apierrors.
-				NewUpstreamError(res.StatusCode).
-				WithDetail(utils.NewJSONResponseError(res.StatusCode, res.Body).OrEmpty().Error()))
-		case strings.HasPrefix(res.Header.Get("Content-Type"), "text/"):
-			return mo.Err[any](apierrors.
-				NewUpstreamError(res.StatusCode).
-				WithDetail(utils.NewTextResponseError(res.StatusCode, res.Body).OrEmpty().Error()))
-		default:
-			slog.Warn("unknown upstream error with unknown Content-Type",
-				slog.Int("status", res.StatusCode),
-				slog.String("content_type", res.Header.Get("Content-Type")),
-				slog.String("content_length", res.Header.Get("Content-Length")),
-			)
+		resError := handleResponseError(res)
+		if resError.IsError() {
+			return resError
 		}
 	}
 
