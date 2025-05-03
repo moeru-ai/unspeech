28f25c865ee16c091d786e8ec5399cbdab3e2680
藍+85CD <50108258+kwaa@users.noreply.github.com>
2025-05-03 13:42:22 +0800

    refactor(provider_openai): use struct

--- crates/provider_openai/src/speech.rs
+++ crates/provider_openai/src/speech.rs
@@ -1,24 +1,44 @@
 use axum::{body::Bytes, http::{header, HeaderMap}};
 use reqwest::Client;
-use serde_json::json;
+use serde::Serialize;
 use unspeech_shared::{
   AppError,
   speech::ProcessedSpeechOptions,
 };
+
+#[derive(Serialize)]
+// https://platform.openai.com/docs/api-reference/audio/createSpeech
+pub struct OpenAISpeechOptions {
+  // The text to generate audio for. The maximum length is 4096 characters.
+  pub input: String,
+  // One of the available TTS models.
+  pub model: String,
+  // The voice to use when generating the audio.
+  pub voice: String,
+  // Control the voice of your generated audio with additional instructions.
+  #[serde(skip_serializing_if = "Option::is_none")]
+  pub instructions: Option<String>,
+  // The format to audio in.
+  #[serde(skip_serializing_if = "Option::is_none")]
+  pub response_format: Option<String>,
+  // The speed of the generated audio.
+  #[serde(skip_serializing_if = "Option::is_none")]
+  pub speed: Option<f32>,
+}
 
 pub async fn handle(
   options: ProcessedSpeechOptions,
   client: Client,
   token: &str,
 ) -> Result<(HeaderMap, Bytes), AppError> {
-  let body = json!({
-    "input": options.input,
-    "model": options.model,
-    "voice": options.voice,
-    "instructions": options.instructions,
-    "response_format": options.response_format,
-    "speed": options.speed,
-  });
+  let body = OpenAISpeechOptions {
+    input: options.input,
+    model: options.model,
+    voice: options.voice,
+    instructions: options.instructions,
+    response_format: options.response_format,
+    speed: options.speed,
+  };
 
   let res = client.post("https://api.openai.com/v1/audio/speech")
     .bearer_auth(token)
